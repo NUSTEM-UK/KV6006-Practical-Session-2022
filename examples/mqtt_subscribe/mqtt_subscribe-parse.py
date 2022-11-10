421b1c23e2011f74ab5ddc350d2542309941be57
jjsanderson <jonathan@quernstone.com>
2022-11-10 00:05:30 +0000

    Working MQTT parsing. Sheesh.

--- examples/mqtt_subscribe/mqtt_subscribe-parse.py
+++ examples/mqtt_subscribe/mqtt_subscribe-parse.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import config
+import json
 
 def on_connect(client, userdata, flags, rc):
     """Callback for when mqtt client connects."""
@@ -10,34 +11,28 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     """Callback for when mqtt client receives a message."""
     topic = msg.topic
-    print(msg.topic)
-
-    try:
-        data = json.loads(msg.payload)
-    except Exception as e:
-        print("Couldn't parse raw data: %s" % msg.paylod, e)
-    print("Made it to here")
-    print(data)
-
-    payload = msg.payload.decode('utf-8')
-    print(payload)
-    data = json.loads(msg.payload)
-
-    print(data)
-    print(msg.payload.decode("utf-8").rstrip().json())
+    # Decode the payload: convert it from bytes to a string...
     payload = msg.payload.decode("utf-8")
-    # For now, output the message
-    print("Received: " + topic + " " + payload)
-    # ...but we'll show how to match a specific topic.
-    # It would usually be better to subscribe to only the desired topic
+    # ...and then parse the string as JSON.
+    data = json.loads(payload)
+    # print(data)
 
-    # data = msg.payload.decode("utf-8").json()
-    # for sensor in data['sensors']:
-    #     print("Sensor Name: " + sensor['name'])
-    #     print("Value: " + sensor["value"])
+    for sensor in data['sensors']:
+        print("Sensor Name: " + sensor['name'])
+        # sensor value is a float, so we need to convert it to a string
+        # Note that error messages are suppressed inside MQTT callbacks,
+        # so it's very easy to mess this up!
+        print("Value: " + str(sensor['value']))
 
     # if (topic.startswith("/KV6006/Sensors/ding")):
     #     print("DING!")
+
+def do_the_thing(incoming):
+    print(incoming)
+    incoming2 = '{"sensors": [{"name": "temperature", "value": 21.42667}]}'
+    # print(incoming2)
+    data = json.loads(incoming)
+    print(data)
 
 # Now we have our callback functions for connection and message receipt,
 # we can go ahead and make a connection.
